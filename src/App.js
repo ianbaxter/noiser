2b4b42ac81a1a219e35c9cfe632a05941bf291a1
Ian <41586905+ianbaxter@users.noreply.github.com>
2019-08-21 00:59:31 +0100

    Add features

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import './index.css';
 import AudioChunk from "./audio";
 
-let audioChunk = new AudioChunk;
+let audioChunk = new AudioChunk();
 
 class App extends Component {
   state = {
@@ -23,14 +23,106 @@ class App extends Component {
     }
   }
 
+  handleChange({ target }) {
+    console.log(target);
+    switch (target.name) {
+      case "freq":
+        audioChunk.changeFreq(target.value)
+        break;
+      case "filter-freq":
+        audioChunk.changeFilterFreq(target.value)
+        break;
+      case "amp":
+        audioChunk.changeAmp(target.value)
+        break;
+      case "lfo":
+        audioChunk.changeLfoSpeed(target.value)
+        break;
+      case "delay-time":
+        audioChunk.changeDelayTime(target.value)
+        break;
+      case "delay-feedback":
+        audioChunk.changeDelayFeedback(target.value)
+        break;
+    }
+  }
+
   render() {
     return (
-      <button 
-        className="btn-play"
-        onClick={() => this.startSynth(this.state.playing)}
-      >
-        {this.state.playing ? "Stop" : "Play"}
-      </button>
+      <div>
+        <button 
+          className="btn-play"
+          onClick={() => this.startSynth(this.state.playing)}
+        >
+          {this.state.playing ? "Stop" : "Play"}
+        </button>
+        <div>
+          <input 
+            type='range' 
+            name="freq" 
+            min='20' 
+            max='8000' 
+            step='1'
+            onInput={ this.handleChange } 
+          />
+          <label for="freq">Osc Frequency</label>
+        </div>
+        <div>
+          <input 
+            type='range' 
+            name="filter-freq" 
+            min='20' 
+            max='2000' 
+            step='1'
+            onInput={ this.handleChange } 
+          />
+          <label for="filter-freq">Filter Frequency</label>
+        </div>
+        <div>
+          <input 
+            type='range' 
+            name="amp" 
+            min='0' 
+            max='1' 
+            step='0.01'
+            onInput={ this.handleChange } 
+          />
+          <label for="amp">Volume</label>
+        </div>
+        <div>
+          <input 
+            type='range' 
+            name="lfo" 
+            min='1' 
+            max='100' 
+            step='1'
+            onInput={ this.handleChange } 
+          />
+          <label for="lfo">Lfo</label>
+        </div>
+        <div>
+          <input 
+            type='range' 
+            name="delay-time" 
+            min='0.1' 
+            max='6' 
+            step='0.1'
+            onInput={ this.handleChange } 
+          />
+          <label for="delay-time">Delay Time</label>
+        </div>
+        <div>
+          <input 
+            type='range' 
+            name="delay-feedback" 
+            min='0' 
+            max='0.9' 
+            step='0.1'
+            onInput={ this.handleChange } 
+          />
+          <label for="delay-feedback">Delay Feedback</label>
+        </div>
+      </div>
     );
   }
 }
